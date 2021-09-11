9efc14ffd4de995b370bfe931040a3994200305a
metialz <74710246+metialz@users.noreply.github.com>
2021-09-11 23:56:12 +0430

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ client.on('message', message => {
 
 	const args = message.content.slice(config.prefix.length).trim().split(/ +/);
 	const cmd = args.shift().toLowerCase();
-
+	if (message.channel.id === '875067223377584198'){
+	message.channel.send("test");
+	}
 
   // verficition start
   if (cmd === 'setup') {
